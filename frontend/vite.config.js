bbc790785ddebe7a1f28f9bd45595e0790e2408b
Agpatil475 <akshaygpatil0811@gmail.com>
2025-01-30 00:52:45 +0530

    added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      'react-pro-sidebar/dist/styles.css': 'node_modules/react-pro-sidebar/dist/styles.css',
+      "react-pro-sidebar/dist/styles.css":
+        "node_modules/react-pro-sidebar/dist/styles.css",
+      "@mui/material/styles": "node_modules/@mui/material/styles", // Add this line
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ["@mui/material/styles"], // Externalize this module
     },
   },
 });
